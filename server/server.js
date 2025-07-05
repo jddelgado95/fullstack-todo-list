9ef9dba89ed04d16aaaa8c3c223e95336b02a698
jddelgado95 <jddelgado95@gmail.com>
2025-07-05 16:31:54 -0600

    Setup express app

---
+++ server/server.js
@@ -0,0 +1,29 @@
+// routes/todos.js
+const express = require("express");
+const router = express.Router();
+const Todo = require("../models/Todo");
+
+router.get("/", async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
+});
+
+router.post("/", async (req, res) => {
+  const todo = new Todo({ title: req.body.title });
+  await todo.save();
+  res.json(todo);
+});
+
+router.put("/:id", async (req, res) => {
+  const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.json(todo);
+});
+
+router.delete("/:id", async (req, res) => {
+  await Todo.findByIdAndDelete(req.params.id);
+  res.json({ success: true });
+});
+
+module.exports = router;
